4ee2708c47424ab59f1f572b94bb44e5254081b3
Willem (Wim) Kaijser <57907547+snwikaij@users.noreply.github.com>
2024-10-08 10:39:21 +0200

    Update Unknown_Kaijser_et_al._GLMM_TP.R

--- Unknown_Kaijser_et_al._GLMM_TP.R
+++ Unknown_Kaijser_et_al._GLMM_TP.R
@@ -3,7 +3,7 @@ library(R2jags)
 library(ggplot2)
 library(mcmcplots)
 
-##To use the whole code JAGS is needed running outside R https://mcmc-jags.sourceforge.io/
+#To use the whole code JAGS is needed running outside R https://mcmc-jags.sourceforge.io/
 
 #download the data
 url      <- "https://raw.githubusercontent.com/snwikaij/Data/main/Unknown_Kaijser_et_al._2025.xlsx"
